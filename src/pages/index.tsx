84f5b5a6b8d394619c7ba5e51e86c1ad82049395
Moisés Eduardo <moises.eduardogc@gmail.com>
2023-11-20 20:50:30 -0300

    penultimo-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import { Menu } from '../../componentes/Menu';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Loja Next</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Menu />
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Livros Next
+                </h1>
+            </main>
+        </div>
+    );
+}
